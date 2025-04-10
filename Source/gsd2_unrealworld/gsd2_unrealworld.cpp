61c42b10b1586149ef5a5f2c345c7d439885904c
이유찬 <delphinium04@outlook.com>
2025-04-11 02:11:15 +0900

    Init

---
+++ Source/gsd2_unrealworld/gsd2_unrealworld.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "gsd2_unrealworld.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, gsd2_unrealworld, "gsd2_unrealworld" );
+ 
